260db1e438d5cf54e81e7fffd6ec947c96c57dd8
mehedihasananik <marshallanique@gmail.com>
2024-03-07 17:26:30 +0600

    whyChoose us completed

--- src/components/Home/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -4,12 +4,12 @@ import React from "react";
 
 const WhyChooseUs = () => {
   return (
-    <div className="WhyChooseUs">
+    <div className="WhyChooseUs my-20">
       <Container>
         {/* whyChoose Us section */}
         <div className="flex justify-between items-center">
           {/* left section */}
-          <div className="md:w-[50%]">
+          <div className="md:w-[55%]">
             {/* title & description */}
             <div>
               <div className="flex items-center gap-4 h-20">
@@ -26,40 +26,60 @@ const WhyChooseUs = () => {
                 coffee beans into a symphony of aromatic notes and rich flavors.
               </p>
             </div>
+
             {/* boxes */}
-            <div>
+            <div className="flex justify-between  py-10">
               {/* 1st box */}
-              <div>
-                <h2>20+</h2>
-                <p>Years Experience</p>
+              <div className="w-[250px] p-10 text-center rounded-lg  border-[#270A05] border-[1.5px] border-opacity-10">
+                <div className="flex justify-center items-center font-Bakilda text-[44px] capitalize font-bold">
+                  <span> 20</span> <span className="text-[20px] ">+</span>
+                </div>
+                <p className="text-[20px] capitalize font-medium">
+                  Years Experience
+                </p>
               </div>
               {/* 2nd box */}
-              <div>
-                <h2>100+</h2>
-                <p>Master Chefs</p>
+              <div className="w-[250px] p-10 text-center rounded-lg  border-[#270A05] border-[1.5px] border-opacity-10">
+                <div className="flex justify-center items-center font-Bakilda text-[44px] capitalize font-bold">
+                  <span> 100</span> <span className="text-[20px] ">+</span>
+                </div>
+                <p className="text-[20px] capitalize font-medium">
+                  Master Chefs
+                </p>
               </div>
               {/* 3rd box */}
-              <div>
-                <h2>30+</h2>
-                <p>Achievements</p>
+              <div className="w-[250px] p-10 text-center rounded-lg  border-[#270A05] border-[1.5px] border-opacity-10">
+                <div className="flex justify-center items-center font-Bakilda text-[44px] capitalize font-bold">
+                  <span> 30</span> <span className="text-[20px] ">+</span>
+                </div>
+                <p className="text-[20px] capitalize font-medium">
+                  Achievements
+                </p>
               </div>
             </div>
+
             {/* description */}
-            <div>
-              <p>
+            <div className="w-[80%]">
+              <p className="text-[16px] font-ValueSans opacity-70">
                 Your choice to savor our coffee is an invitation to experience
                 the epitome of craftsmanship, flavor, and dedication.
               </p>
             </div>
             {/* button */}
-            <div>
-              <button>Explore Our Menus</button>
+            <div className="my-5 ">
+              <button
+                className="text-[#86371C] text-[16px] font-ValueSans
+               border-[2px] border-[#86371C] rounded-[12px] px-5 py-2 font-semibold"
+              >
+                Explore Our Menus
+              </button>
             </div>
           </div>
 
           {/* right section */}
-          <div className="md:w-[50%] flex justify-end">
+          <div className="md:w-[50%] flex justify-center pt-14">
             <Image
+              className="ml-4"
               src={"/images/whyUs (2).png"}
               width={450}
               height={500}
